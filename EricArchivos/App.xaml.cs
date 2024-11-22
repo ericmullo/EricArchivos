c7da23b73ba0c6fc2e647e3de872a352ed64e7c2
laboratorio <laboratorio@UPOAULA10620>
2024-11-22 07:44:41 -0500

    Agregar archivos de proyecto.

---
+++ EricArchivos/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EricArchivos
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
